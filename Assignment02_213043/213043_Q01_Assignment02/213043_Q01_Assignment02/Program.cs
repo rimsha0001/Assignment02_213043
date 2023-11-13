604d13acdaace49bd7d85d227349e2dfa91b20ec
Rimsha <merimsha8@gmail.com>
2023-11-13 23:50:17 +0500

    first commit

---
+++ Assignment02_213043/213043_Q01_Assignment02/213043_Q01_Assignment02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml.Linq;
+//c)
+namespace Qno1
+{ 
+
+    abstract class Telephone
+    {
+        protected string phonetype;
+        public abstract void Ring();
+
+    }
+    class Digitalphone : Telephone
+    {
+       public Digitalphone() 
+       {
+            phonetype = "Digital";
+
+       }
+
+        public override void Ring()
+        {
+            // Implement how the DigitalPhone rings.
+            Console.WriteLine($" Ringing the {phonetype} phone");
+        }
+    }
+    class Talkingphone : Telephone 
+    {
+        public Talkingphone()
+        {
+            phonetype = "Talking";
+        }
+
+        public override void Ring()
+        {
+            // Implement how the DigitalPhone rings.
+            Console.WriteLine($" Ringing the {phonetype} phone");
+        }
+
+    }
+    class program 
+    {
+        static void Main (string[] args)
+        {
+            Digitalphone digitalphone = new Digitalphone();
+            Talkingphone talkingphone = new Talkingphone();
+            digitalphone.Ring();
+            talkingphone.Ring();
+
+
+
+        }
+    }
+}
